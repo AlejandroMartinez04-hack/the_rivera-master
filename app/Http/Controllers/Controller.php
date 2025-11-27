6201385ccb9e42e729261d8b4bd57aeaba037bcf
Alejandro Martinez Luz <s22030116@itsch.edu.mx>
2025-11-27 11:02:06 -0600

    INTENTO3

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,7 +10,6 @@ namespace App\Http\Controllers;
  *         email="s22030116@itsch.edu.mx"
  *     )
  * )
-*@OA\Server(url="http://localhost:8000") 
 * @OA\Server(url="https://therivera-master-production.up.railway.app")
  * @OA\SecurityScheme(
  *     securityScheme="bearer_token",
